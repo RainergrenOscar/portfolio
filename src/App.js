506c591f0bbd4cbafa956e9fb17949a7785c30ae
RainergrenOscar <oscar.rainergren@hotmail.com>
2022-01-11 17:34:27 +0100

    Made Navbar work and added SASS

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
-import './App.css';
-
+import { BrowserRouter, Route, Routes,NavLink,Navigate } from 'react-router-dom';
+import Layout from './components/layouts/Layout';
+import Test1 from './pages/Test1';
+import Test2 from './pages/Test2';
 function App() {
   return (
-    <div className="App">
-      
-    </div>
+    <Layout>
+        <Routes>
+        <Route path="/test-1" element={<Test1 />} />
+        <Route path="/test-2" element={<Test2/>}/>
+        </Routes>
+    </Layout>
   );
 }
 
