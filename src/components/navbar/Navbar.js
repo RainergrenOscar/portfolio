827c68f600ef4491d022eb53ba6062ce03bce89c
RainergrenOscar <oscar.rainergren@hotmail.com>
2022-01-12 15:15:20 +0100

    fixed some ui problems

--- src/components/navbar/Navbar.js
+++ src/components/navbar/Navbar.js
@@ -2,6 +2,7 @@ import React, {useState,useEffect} from 'react'
 import { BiMenuAltRight } from "react-icons/bi"
 import { AiOutlineClose } from "react-icons/ai"
 import { MdLightbulb } from "react-icons/md";
+import { FiSun,FiMoon } from "react-icons/fi";
 
 
 import classes from "./Navbar.module.scss"
@@ -9,6 +10,7 @@ import { NavLink } from 'react-router-dom'
 
 const Navbar = () => {
     const [menuOpen, setMenuOpen] = useState(false) //setting state to change navbar on smaller screen
+    const [darkMode, setDarkMode] = useState(false)
     const [size, setSize] = useState({
         width: undefined,
         height: undefined
@@ -35,6 +37,14 @@ const Navbar = () => {
         setMenuOpen((p) => !p)
     }
 
+
+    const darkTime = () => {
+        setDarkMode((e) => !e)
+    }
+
+
+
+
     return (
         <header className={classes.header}>
             <div className={classes.header__content}>
@@ -44,7 +54,7 @@ const Navbar = () => {
             <nav className={`${classes.header__content__nav} ${menuOpen && size.width < 768 ? classes.isMenu : ""}`}>
                 <ul>
                     <li>
-                        <NavLink to="/about" onClick={menuToggleHandler}>About</NavLink>
+                        <NavLink to="/contact" onClick={menuToggleHandler}>Contact</NavLink>
                     </li>
                     <li>
                         <NavLink to="/projects" onClick={menuToggleHandler}>Projects</NavLink>
@@ -52,7 +62,11 @@ const Navbar = () => {
                     <li>
                         <NavLink to="/resume" onClick={menuToggleHandler}>Resume</NavLink>
                         </li>  
-                </ul>
+                    </ul>
+                    <div className={classes.header__content__darkmode}>
+                        {!darkMode ? (<FiSun className={classes.header__content__sun} onClick={darkTime} />) :
+                            (<FiMoon className={classes.header__content__moon} onClick={darkTime} />)}
+                    </div>
             </nav>
             <div className={classes.header__content__toggle}>
                     {!menuOpen ? (<BiMenuAltRight onClick={menuToggleHandler}/>) : (<AiOutlineClose onClick={menuToggleHandler}/>)}
