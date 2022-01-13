71360e4f396e74efd02624dc522a10670ce22f41
RainergrenOscar <oscar.rainergren@hotmail.com>
2022-01-13 14:47:41 +0100

    added basic laoyut to landing page

--- src/pages/LandingPage.js
+++ src/pages/LandingPage.js
@@ -1,5 +1,7 @@
 import classes from "./LandingPage.module.scss"
-import GuyInChair from "../img/desktopboy.svg"
+import GuyInChair from "../img/peep-54.svg"
+import ProfilePicture from "../img/Profile.png"
+
 
 
 const LandingPage = () => {
@@ -10,7 +12,15 @@ const LandingPage = () => {
                     <img src={GuyInChair} className={classes.section__container__item__img} alt="Guy sitting at computer table" />
                     <div className={classes.section__container__item__text}><p>Hi! I am a future Front-End Developer who is currently studying in Stockholm</p></div>
                 </div>
-
+                <div className={classes.section__container__split}>
+                    <div className={classes.section__container__split__name}>
+                        <h1>Oscar Rainergren</h1>
+                        <p>(Front-end Developer based in Stockholm)</p>
+                    </div>
+                    <div className={classes.section__container__split__img}>
+                        <img src={ProfilePicture} className={classes.section__container__split__img}alt="Guy face profile picture" />
+                    </div>
+                </div>
             </div>
         </section>
         
