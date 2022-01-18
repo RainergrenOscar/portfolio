d6104d2f68bb2eccb7b9e527945080e2b5e36844
RainergrenOscar <oscar.rainergren@hotmail.com>
2022-01-18 16:54:57 +0100

    started on contact

--- src/components/cards/Data.js
+++ src/components/cards/Data.js
@@ -6,25 +6,28 @@ export const ProjectsData = [
         image: "https://camo.githubusercontent.com/c9073913d0e732f7b103c3fa1f62c74eedeac1cb7c1be4eb1d42fe50d007b86e/68747470733a2f2f692e706f7374696d672e63632f5a6e5a4c4c3337302f536b612d726d617662696c642d323032322d30312d30392d6b6c2d31352d35372d30372e706e67",
         title: "A Kanye West Memory",
         description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Minus doloribus, adipisci distinctio doloremque aperiam omnis illo tenetur perferendis",  
+        link: "https://github.com/RainergrenOscar/Memory"
     },
     {
         id: 1,
         image: "https://mobilanyheter.net/wp-content/uploads/2020/06/783px-Test-Logo.svg_-5.png",
         title: "Gifted. E-commerce",
         description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Minus doloribus, adipisci distinctio doloremque aperiam omnis illo tenetur perferendis",  
-        
+        link: "https://github.com/RainergrenOscar/grupp4"
     },
     {
         id: 2,
         image: "https://mobilanyheter.net/wp-content/uploads/2020/06/783px-Test-Logo.svg_-5.png",
         title: "Planet F1",
         description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Minus doloribus, adipisci distinctio doloremque aperiam omnis illo tenetur perferendis",
+        link: "https://github.com/RainergrenOscar/Planet-F1"
     },
     {
         id: 3,
         image: "https://mobilanyheter.net/wp-content/uploads/2020/06/783px-Test-Logo.svg_-5.png",
         title: "Lorem Ipsum",
         description: "I look so dumb right noooooooow, u better hurry up",
+        link: "https://github.com/RainergrenOscar/Watch-Co"
     }
 ]
     
@@ -34,13 +37,14 @@ export const HighlightProjects = [
         image: "https://camo.githubusercontent.com/c9073913d0e732f7b103c3fa1f62c74eedeac1cb7c1be4eb1d42fe50d007b86e/68747470733a2f2f692e706f7374696d672e63632f5a6e5a4c4c3337302f536b612d726d617662696c642d323032322d30312d30392d6b6c2d31352d35372d30372e706e67",
         title: "A Kanye West Memory",
         description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Minus doloribus, adipisci distinctio doloremque aperiam omnis illo tenetur perferendis",  
+        link: "https://github.com/RainergrenOscar/Memory"
     },
     {
         id: 1,
         image: "https://mobilanyheter.net/wp-content/uploads/2020/06/783px-Test-Logo.svg_-5.png",
         title: "Gifted. E-commerce",
         description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Minus doloribus, adipisci distinctio doloremque aperiam omnis illo tenetur perferendis",  
-        
+        link: "https://github.com/RainergrenOscar/grupp4"
     }  
 ]
     
