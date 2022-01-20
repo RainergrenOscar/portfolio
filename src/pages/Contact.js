468a2ac453b2989f397027eea404fd52f516c372
RainergrenOscar <oscar.rainergren@hotmail.com>
2022-01-20 11:58:46 +0100

    contact page yupdated

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -2,9 +2,7 @@ import React, { useState } from 'react'
 import classes from "./Contact.module.scss"
 import emailjs from "emailjs-com"
 import { BsFacebook, BsGithub, BsInstagram, BsLinkedin } from 'react-icons/bs'
-import { Link } from 'react-router-dom'
-import { IoLocationSharp,IoMail } from "react-icons/io5"
-import { AiTwotonePhone } from 'react-icons/ai'
+
 
 
 
@@ -25,36 +23,43 @@ const [email, setEmail] = useState(false)
 
 
     return (
-        <section className={classes.container}>
-            <div className={classes.container__title}>
-                <h1>CONTACT</h1>
-                <p>Lorem, ipsum dolor sit amet consectetur adipisicing.</p>
+        <section>
+            <div className={classes.container}>
+                <div className={classes.container__split}>
+                    <div>
+                        <h1>Contact me.</h1>
+                        <p>Let’s have a conversation! I’d love to hear about what you’re working on and find a way to work together.</p>
+                        <div className={classes.container__split__icons}>
+                            <div >
+                               <a href=""><BsFacebook  /></a> 
+                            </div>
+                            <div >
+                                <a href=""><BsLinkedin /></a>
+                            </div>
+                            <div >
+                                <a href=""><BsGithub /></a>
+                            </div>
+                        </div>
+                       
+                    </div>
+                    <div>
+                        <form action="/">
+                            <div className={classes.form__container}>
+                                <input type="text" placeholder='Name' />
+                            </div>
+                            <div className={classes.form__container}>
+                                <input type="text" placeholder='Email' />
+                            </div>
+                            <div className={classes.form__container}>
+                                <textarea placeholder='Message'></textarea>
+                            </div>
+                            <div className={classes.form__container__button}>
+                            <button className={classes.form__container__button__center}>Send</button>
+                            </div>
+                        </form>
+                    </div>
+                </div>
 
-                <form onSubmit={sendEmail}>
-                    <div className={classes.container__form}>
-                        <label htmlFor="user_name">Name</label>
-                        <input type="text" name='user_name' placeholder='Name'className={classes.container__form__element} required />
-                    </div>
-                    <div className={classes.container__form}>
-                        <label htmlFor="user_lastname">Lastname</label>
-                        <input type="text" name='user_lastname' placeholder='Lastname'className={classes.container__form__element} required/>
-                    </div>
-                    <div className={classes.container__form}>
-                        <label htmlFor="user_mail">Mail</label>
-                        <input type="text" name='user_mail' placeholder='Mail'className={classes.container__form__element}required />
-                    </div>
-                    <div className={classes.container__form}>
-                        <label htmlFor="user_telephone">Tel</label>
-                        <input type="text" name='user_telephone' placeholder='Phonenumber'className={classes.container__form__element} required />
-                    </div>
-                    <div className={classes.container__form__full}>
-                        <label htmlFor="user_message">Message</label>
-                        <textarea type="text-area" name='user_message' placeholder="Message"className={classes.container__form__full__textarea} required/>
-                    </div>
-                    <div className={classes.container__submit}>
-                        <button type='submit' value="Send">Send</button>
-                    </div>
-                </form>
             </div>
         </section>
     )
